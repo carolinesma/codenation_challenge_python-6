4b2188e522ee0f0deae4700a0a8ab8ef255e8cd3
Caroline Alves <sma.caroline@ymail.com>
2020-05-27 01:31:13 -0300

    Alteração até o íten 5

--- main.py
+++ main.py
@@ -8,31 +8,45 @@ class Department:
 
 class Employee:
     def __init__(self, code, name, salary):
-        self.code = code
-        self.name = name
-        self.salary = salary
+        self.__code = code
+        self.__name = name
+        self.__salary = salary
 
     def calc_bonus(self):
         pass
 
     def get_hours(self):
         pass
+
+    def set_employee(self, code, name, salary):
+        self.code = code
+        self.__name = name
+        self.salary = salary
 
 
 class Manager(Employee):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self.departament = Department('managers', 1)
+        self.__department = Department('managers', 1)
 
     def calc_bonus(self):
         return self.salary * 0.15
 
+    def get_departament(self):
+        return self.__department
 
-class Seller(Manager):
+
+class Seller(Employee):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
         self.departament = Department('sellers', 2)
-        self.sales = 0
+        self.__sales = 0
 
     def get_hours(self):
         return 6
+
+    def get_sales(self):
+        return self.__sales
+
+    def put_sales(self, sales):
+        self.__sales += sales
